3622f02b519faf0eb0df2370ce9cf304353d3351
Mirza Andira Nugraha <114473359+mirzaandira@users.noreply.github.com>
2024-03-21 13:40:08 +0700

    Add files via upload

---
+++ App.java
@@ -0,0 +1,12 @@
+import bangundatar.*;
+import shape.*;
+
+public class App {
+    public static void main(String[] args) {
+        Persegi A = new Persegi(5);
+        Square B = new Square(5);
+
+        A.getInfo();
+        B.getInfo();
+    }
+}
